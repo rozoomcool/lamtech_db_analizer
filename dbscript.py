a0af3f1641df8bea83febb29984b4c25152b0264
rozoomcool <rozoom.um@gmail.com>
2023-11-04 01:53:23 +0300

    fixes

--- dbscript.py
+++ dbscript.py
@@ -1,6 +1,30 @@
-import psycopg2
+import time
 
-def check_db():
-    conn  = psycopg2.connect(
-        
-    )
+import asyncpg
+
+async def checking_db():
+    while True:
+        await sleep(1000 * 60)
+        await check_db()
+
+        async def check_db():
+            try:
+                conn = await asyncpg.connect(
+                    host="5.53.124.214",
+                    port="5432",
+                    database="lamtech_db",
+                    user="postgres",
+                    password="root"
+                )
+
+                await conn.executed("SELECT * FROM pg_stat_activity;")
+                await conn.close()
+                await bot.send_message("База данных работает отлично")
+
+            except (asyncpg.PostgresError, ConnectionError) as e:
+                await bot.send_message("Произошел сбой в работе базы данных:", e)
+
+
+def status(update,  context):
+    db_status = check_db()
+    update.message.reply_text(f"Current DB status: {db_status}")
--- main.py
+++ main.py
@@ -61,5 +61,5 @@ async def echo(message: types.Message):
 
 
 if __name__ == '__main__':
-
+    dp.loop.create_task(check_bd())
     executor.start_polling(dp, skip_updates=True)
