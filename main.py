4d360e6c13ae6134f4e3698f7ce78da6acaaf7d0
Dikiliev <magamed773377@gmail.com>
2023-11-03 14:34:55 +0300

    aiogram

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-import os
+from aiogram import Bot, Dispatcher, executor, types
 
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+API_TOKEN = '6526338938:AAH5fJNLx3iqWqfjwv3z6dF7KREmy1fmOEU'
 
-import config
-
-
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
 
 
-app = ApplicationBuilder().token(os.environ['TOKEN']).build()
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет! Я твой бот на aiogram.")
 
-app.add_handler(CommandHandler("start", hello))
 
-app.run_polling()
+@dp.message_handler(commands=['help'])
+async def send_help(message: types.Message):
+    await message.reply("Список команд: /start, /help")
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+# Запускаем бота
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
