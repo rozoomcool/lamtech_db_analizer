4f95b75bc43b56e4a190d9cdf87a4fc10b8aee27
eyekeen <block-code@mail.ru>
2023-11-05 04:15:57 +0300

    add log output

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import psutil
 from dotenv import dotenv_values
 import bench_requests
 import analyzer.slow_query
+import logs.logs
 
 # TODO: errors handler
 
@@ -26,9 +27,8 @@ keyboard.add(
     types.InlineKeyboardButton(text='Информация о системе'),
     types.InlineKeyboardButton(text='Перезагрузить БД'),
     types.InlineKeyboardButton(text='Вывод логов'),
-    types.InlineKeyboardButton(text='Легкое тестирование'),
-    types.InlineKeyboardButton(text='Медленные запросы'),
-    types.InlineKeyboardButton(text='Реально крашнуть базу данных!'),
+    types.InlineKeyboardButton(text='Тестирование'),
+    types.InlineKeyboardButton(text='Проверить время выполнения запросов'),
 )
 
 
@@ -59,13 +59,13 @@ async def check_bd(message: types.Message):
             user="postgres",
             password="root"
         )
-        await message.answer("Сервер работает в штатном режиме", reply_markup=keyboard)
+        await message.answer("БД работает в штатном режиме", reply_markup=keyboard)
     except Exception as e:
         print(e)
-        await message.answer("База данных не отвечает", reply_markup=keyboard)
+        await message.answer("БД не отвечает", reply_markup=keyboard)
 
 
-@dp.message_handler(lambda message: message.text == 'Проверить работу сервера')
+@dp.message_handler(lambda message: message.text == 'Проверить работу БД')
 async def check_server(message: types.Message):
     await check_bd(message)
 
@@ -99,10 +99,13 @@ async def server_info(message: types.Message):
 
 @dp.message_handler(lambda message: message.text == 'Вывод логов')
 async def logs_btn(message: types.Message):
-    await message.answer('logs')
+    file = logs.logs.gen_logs()
+    with open (f"./{file}", "rb") as log_file:
+        await bot.send_document(chat_id=message.chat.id, document=log_file)
 
 
-@dp.message_handler(lambda message: message.text == 'Легкое тестирование')
+
+@dp.message_handler(lambda message: message.text == 'Тестирование')
 async def easy_test_btn(message: types.Message):
     await message.answer('Начинаю нагрузочный тест...')
     data = bench_requests.easy_test()
@@ -116,43 +119,21 @@ async def easy_test_btn(message: types.Message):
         await message.answer(params)
 
 
-@dp.message_handler(lambda message: message.text == 'Медленные запросы')
+@dp.message_handler(lambda message: message.text == 'Проверить время выполнения запросов')
 async def hard_test_btn(message: types.Message):
     await message.answer('Начинаю сбор информации...')
     data = analyzer.slow_query.get_tables_list(analyzer.slow_query.get_conn())
     res = analyzer.slow_query.check_query_time(analyzer.slow_query.get_conn(), data)
     for table in res:
         query_execute_time = 'Медленно' if float(res[table]['execution_time']) >= 2 else 'Быстро' # итоговая оценка скорость выполнения запроса
-        await message.answer(f"Таблица: {table} \n Запрос: ```sql f{res[table]['executed_query']} ``` \n Ожидаемое время выполнения запроса: {res[table]['planning_time']} \n Время выполнения запроса: {res[table]['execution_time']} \n Итоговая оценка скорость выполнения запроса(время в 2 миллисекунды используеться для примера): {query_execute_time}")
+        await message.answer(f"Таблица: {table} \n Запрос: ```sql f{res[table]['executed_query']} ``` \n Ожидаемое время выполнения запроса: {res[table]['planning_time']} мл \n Время выполнения запроса: {res[table]['execution_time']} мл \n Итоговая оценка скорость выполнения запроса(время в 2 миллисекунды используются для примера): {query_execute_time}")
 
-@dp.message_handler(lambda message: message.text == 'Реально крашнуть базу данных!')
-async def crush_btn(message: types.Message):
-    await message.answer('Убиваем БД, ждите')
-    if(bench_requests.crush_db() != 200):
-        await message.answer('Произошла непредвиденная ошибка')
-    else:
-        await message.answer('БД убита')
-
-@dp.message_handler(state='*', commands=['cancel'])
-async def cancel_handler(message: types.Message, state: FSMContext):
-    """Allow user to cancel action via /cancel command"""
-
-    current_state = await state.get_state()
-    if current_state is None:
-        # User is not in any state, ignoring
-        return
-
-    # Cancel state and inform user about it
-    await state.finish()
-    await message.reply('Cancelled.')
 
 async def db_ok():
     await users.send_message(bot, 'База данных работает в штатном режиме')
 
-
 async def db_failed():
     await users.send_message(bot, 'Внимание! Произошел сбой в работе базы данных:')
-
 
 async def check_db_exec(_):
     asyncio.create_task(dbscript.checking_db(db_ok, db_failed))
